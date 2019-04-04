16c4ec9c5ea588e8b3e21e3a9651e4150715b12a
Vladislav Ovcharenko <vladislav.ovcharenko@powerhouse.nl>
2019-04-04 22:13:16 +0200

    token to header

--- actions.go
+++ actions.go
@@ -20,6 +20,10 @@ type Action struct {
 	Timestamp   string
 }
 
+type ActionResponse struct {
+	Token string
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "actions"
@@ -27,13 +31,19 @@ func main() {
 	app.Version = "1.0.0"
 	app.EnableBashCompletion = true
 
+	var activeToken string
+
 	app.Commands = []cli.Command{
 		{
 			Name:    "all",
 			Aliases: []string{"a"},
 			Usage:   "get list of actions",
 			Action: func(c *cli.Context) error {
-				actions := getAllActions()
+				if !isAuthorized(activeToken) {
+					fmt.Printf("Unauthorized, you have to log in first!\n")
+					return nil
+				}
+				actions := getAllActions(activeToken)
 				printActions(actions)
 				return nil
 			},
@@ -47,9 +57,30 @@ func main() {
 				cli.StringFlag{Name: "result, r"},
 			},
 			Action: func(c *cli.Context) error {
+				if !isAuthorized(activeToken) {
+					fmt.Printf("Unauthorized, you have to login first!\n")
+					return nil
+				}
 				saved := createAction(c.String("desc"), c.String("result"))
 				printDelimiter()
 				printAction(saved)
+				return nil
+			},
+		},
+		{
+			Name:    "login",
+			Aliases: []string{"n"},
+			Usage:   "log in into app",
+			Flags: []cli.Flag{
+				cli.StringFlag{Name: "user, u"},
+				cli.StringFlag{Name: "password, pw"},
+			},
+			Action: func(c *cli.Context) error {
+				token := authenticate(c.String("user"), c.String("password"))
+				activeToken = token
+				cli.String("token") := activeToken
+
+				fmt.Printf("Token received %s\n", activeToken)
 				return nil
 			},
 		},
@@ -61,10 +92,34 @@ func main() {
 	}
 }
 
-func getAllActions() []Action {
-	response, err := http.Get("http://localhost:8080/api/item")
+func authenticate(username string, password string) string {
+	jsonData := map[string]string{"username": username, "password": password}
+	jsonValue, _ := json.Marshal(jsonData)
+	response, err := http.Post("http://localhost:8080/auth", "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
-		fmt.Printf("Request to notes storage failed with error %s\n", err)
+		fmt.Printf("Failed to authenticate %s\n", err)
+		return ""
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+
+	var ar ActionResponse
+	json.Unmarshal([]byte(data), &ar)
+
+	return ar.Token
+}
+
+func getAllActions(token string) []Action {
+	req, err := http.NewRequest("GET", "http://localhost:8080/api/item", nil)
+	if err != nil {
+		log.Fatal("Error reading request. ", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	client := &http.Client{}
+	response, err := client.Do(req)
+
+	if err != nil {
+		fmt.Printf("Request to actions storage failed with error %s\n", err)
 		return nil
 	}
 	data, _ := ioutil.ReadAll(response.Body)
@@ -80,13 +135,17 @@ func createAction(desc string, result string) Action {
 	jsonValue, _ := json.Marshal(jsonData)
 	response, err := http.Post("http://localhost:8080/api/item", "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
-		fmt.Printf("Request to notes storage failed with error %s\n", err)
+		fmt.Printf("Request to actions storage failed with error %s\n", err)
 		// return nil
 	}
 	data, _ := ioutil.ReadAll(response.Body)
 	var action Action
 	json.Unmarshal([]byte(data), &action)
 	return action
+}
+
+func isAuthorized(token string) bool {
+	return len(token) > 0
 }
 
 func printActions(actions []Action) {
